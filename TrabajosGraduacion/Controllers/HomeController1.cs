0229c660d2b43ac1e468d0b9f1add5b458ec15f6
Guillermo <alexguerrero3a91@gmail.com>
2020-11-26 20:01:10 -0600

    buscador hasta donde pude porque no hay base aun para los parametros y demas vistas

---
+++ TrabajosGraduacion/Controllers/HomeController1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrabajosGraduacion.Controllers
+{
+    public class HomeController1 : Controller
+    {
+        private ConexionBD mibd = new ConexionBD();
+        public ActionResult Index(string cadena="")
+        {
+            List<Datos> datos = mibd.buscarDatos(cadena);
+            return View(datos);
+        }
+    }
+}
