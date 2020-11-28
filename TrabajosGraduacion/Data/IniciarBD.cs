b28bb274e3f2c44e7d639cb59caafccaf3ef9d02
César <cesrsto@gmail.com>
2020-11-28 03:16:14 -0600

    Hoy si :'v
    Solo el combobox no quiere guardar xd

---
+++ TrabajosGraduacion/Data/IniciarBD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrabajosGraduacion.Data
+{
+    public class IniciarBD
+    {
+        public static void Iniciar(TrabajosGraduacionContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Registro.Any())
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ TrabajosGraduacion/Models/Registro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrabajosGraduacion.Models
+{
+    public class Registro
+    {
+        public int RegistroID { get; set; }
+        [Required]
+        public string Tipo { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        [DisplayFormat(DataFormatString = "{0:yyyy}")]
+        public DateTime Año { get; set; }
+        public string Facultad { get; set; }
+        public string Carrera { get; set; }
+    }
+}
